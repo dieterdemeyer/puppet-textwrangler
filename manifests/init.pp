2d6875c44e3a7c03fbbd4e02abde94ce8162829b
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-24 13:35:43 +0100

    Added ensure param

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,9 @@
 # Examples
 #
 #  include textwrangler
-class textwrangler {
+class textwrangler($ensure='present') {
   package { 'TextWrangler':
+    ensure   => $ensure,
     provider => 'appdmg',
     source   => 'http://ash.barebones.com/TextWrangler_4.5.dmg',
   }
