2fa1eea7bc452287e88171c9c2d851b4a43bb6cb
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-24 13:50:45 +0100

    Removed ensure param because provider doesn't support removal of packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,8 @@
 # Examples
 #
 #  include textwrangler
-class textwrangler($ensure='present') {
+class textwrangler {
   package { 'TextWrangler':
-    ensure   => $ensure,
     provider => 'appdmg',
     source   => 'http://ash.barebones.com/TextWrangler_4.5.dmg',
   }
